e9b8b65a2f9f7056c95cb466a858472477b96549
Aritra-Dey-117-XT <ece22127@iiitkalyani.ac.in>
2024-06-26 12:08:26 +0530

    Fixed: Missing Credentials for PLAIN

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -38,7 +38,7 @@ export const sendEmail = async({email, emailType, userID}: any) => {
             port: 587,
             secure: false,
             auth: {
-                user: process.env.TASKNEST,
+                user: process.env.TASKNEST_EMAIl,
                 pass: process.env.TASKNEST_APP_PASSWORD
             }
         });
